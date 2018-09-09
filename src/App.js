6b9a3e739344f7027e557398b34406f66e907040
NateRive <naterive1125@gmail.com>
2018-09-09 14:23:51 +0900

    bigin create app from scratch

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+import BurgerBulder from './containers/BuegerBuilder/BurgerBuilder';
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<Layout>
+					<BurgerBulder />
+				</Layout>
+			</div>
+		);
+	}
+}
+
+export default App;
