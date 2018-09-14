352f14b6c66f759ce07bbfbf11f075d5df82120a
NateRive <naterive1125@gmail.com>
2018-09-14 09:21:00 +0900

    improve perfomance by preventing ordersummary compo from unnecessary re-rendering

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,33 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-	const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
+class OrderSummary extends Component {
+	//This could be a functionla component, doesn't have to be a class
+	componentWillUpdate() {
+		console.log('[OrderSummary] willUpdate');
+	}
+
+	render() {
+		const ingredientSummary = Object.keys(this.props.ingredients).map((igKey) => {
+			return (
+				<li key={igKey}>
+					<span style={{ textTransform: 'capitalize' }}>{igKey}</span> :{this.props.ingredients[igKey]}
+				</li>
+			);
+		});
+
 		return (
-			<li key={igKey}>
-				<span style={{ textTransform: 'capitalize' }}>{igKey}</span> :{props.ingredients[igKey]}
-			</li>
+			<Aux>
+				<h3>Your order</h3>
+				<p>A delicious burger with the following ingredients</p>
+				<ul>{ingredientSummary}</ul>
+				<p>
+					<strong>Total PRice: {this.props.price.toFixed(2)} </strong>
+				</p>
+				<p>Continue to Checkout?</p>
+				<Button btnType="Danger" clicked={this.props.purchaseCanceled}>
+					Cancel
+				</Button>
+				<Button btnType="Success" clicked={this.props.purchaseContinued}>
+					Continue
+				</Button>
+			</Aux>
 		);
-	});
+	}
+}
 
-	return (
-		<Aux>
-			<h3>Your order</h3>
-			<p>A delicious burger with the following ingredients</p>
-			<ul>{ingredientSummary}</ul>
-			<p>
-				<stront>Total PRice: {props.price.toFixed(2)} </stront>
-			</p>
-			<p>Continue to Checkout?</p>
-			<Button btnType="Danger" clicked={props.purchaseCanceled}>
-				Cancel
-			</Button>
-			<Button btnType="Success" clicked={props.purchaseContinued}>
-				Continue
-			</Button>
-		</Aux>
-	);
-};
-
-export default orderSummary;
+export default OrderSummary;
